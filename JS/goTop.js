39a0ef76d28779826cd8bb044dde7b5d163ea005
DAN3002 <dinhanh300229@gmail.com>
2018-12-17 13:40:30 +0700

    Add Go to top

---
+++ JS/goTop.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+	init();
+
+	if($(window).width() >= 768)
+	{
+		$(window).scroll(function()
+		{ 
+			init();
+	    }); 		
+	}
+
+	if($(window).width() >= 992)	
+	{
+	    $("#goTop").hover(function(){
+	    	$(".popup-text").fadeIn();
+	    }, function(){
+	    	$(".popup-text").fadeOut();
+	    });  		
+	}    
+    $("#goTop").click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    });	
+});
+
+function init()
+{
+    if ($(this).scrollTop() > 600)
+    {
+    	$("#goTop").fadeIn();
+    } 
+    else 
+    {
+    	$("#goTop").fadeOut(); 
+    	$(".popup-text").fadeOut();
+    }
+}
