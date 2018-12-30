39967d6d28353ed6a537021c9c7af7736effef79
DAN3002 <dinhanh300229@gmail.com>
2018-12-30 23:20:06 +0700

    Final LichSu

---
+++ JS/changer.js
@@ -0,0 +1,47 @@
+var title =
+[
+	"Tiền Thăng Long",
+	"Thăng Long",
+	"Nhà Nguyễn và Pháp thuộc",
+	"2 cuộc chiến tranh",
+	"Hà Nội đương đại"
+];
+
+var text =
+[
+	"Những di chỉ khảo cổ tại Cổ Loa cho thấy con người đã xuất hiện ở khu vực Hà Nội từ cách đây 2 vạn năm, giai đoạn của nền văn hóa Sơn Vi. Các hiện vật khảo cổ giai đoạn tiếp theo, từ đầu thời đại đồ đồng đến đầu thời đại đồ sắt, minh chứng cho sự hiện diện của Hà Nội ở cả bốn thời đại văn hóa: Phùng Nguyên, Đồng Đậu, Gò Mun và Đông Sơn. Những cư dân Hà Nội thời kỳ đó sinh sống nhờ trồng trọt, chăn nuôi và chài lưới. Giai đoạn tiền sử này tương ứng với thời kỳ của các Vua Hùng trong truyền thuyết. Thế kỷ III trước Công Nguyên, trong cuộc chiến với quân Tần từ phương Bắc, Thục Phán quyết định đóng đô ở Cổ Loa, nay là huyện Đông Anh, cách trung tâm Hà Nội khoảng 15 km. Sự xuất hiện của thành Cổ Loa ghi dấu Hà Nội lần đầu tiên trở thành một đô thị trung tâm về chính trị và xã hội.",
+	"Sau khi lên ngôi năm 1009 tại Hoa Lư, năm 1010, Lý Thái Tổ quyết định dời đô về Đại La. Theo một truyền thuyết phổ biến, khi tới Đại La, Lý Thái Tổ nhìn thấy một con rồng bay lên, vì vậy đặt tên kinh thành mới là Thăng Long. Kinh thành Thăng Long khi đó giới hạn bởi ba con sông: sông Hồng ở phía Đông, sông Tô phía Bắc và sông Kim Ngưu phía Nam. Khu hoàng thành được xây dựng gần hồ Tây với cung điện hoàng gia cùng các công trình chính trị. Phần còn lại của đô thị là những khu dân cư, bao gồm các phường cả nông nghiệp, công nghiệp và thương nghiệp. Ngay trong thế kỷ X, nhiều công trình tôn giáo nhanh chóng được xây dựng, chùa Diên Hựu phía Tây hoàng thành xây năm 1049, chùa Báo Thiên xây năm 1057, Văn Miếu xây năm 1070, Quốc Tử Giám dựng năm 1076... Chỉ sau một thế kỷ, Thăng Long trở thành trung tâm văn hóa, chính trị và kinh tế của cả quốc gia.",
+	"Đến năm 1902, Hà Nội trở thành thủ đô của toàn Liên bang Đông Dương. Nhờ sự quy hoạch của người Pháp, thành phố dần có được bộ mặt mới. Lũy thành thời Nguyễn dần bị triệt hạ, đến năm 1897 hầu như bị phá hủy hoàn toàn, chỉ còn lại Cột Cờ, Cửa Bắc với vết đạn năm 1873, Đoan Môn và lan can rồng đá ở trong hoàng thành cũ. Năm 1901, các công trình phủ Thống sứ, Nhà bưu điện, Kho bạc, Nhà đốc lý, Nhà hát lớn, Cầu Long Biên, Ga Hà Nội, những quảng trường, bệnh viện... được xây dựng. Hà Nội cũng có thêm trường đua ngựa, các nhà thờ Cơ Đốc giáo, trường Đại học Y khoa, Đại học Đông dương, Đại học Mỹ thuật, các trường Cao đẳng Pháp lý, Nông lâm cùng những nhà máy sản xuất rượu bia, diêm, hàng dệt, điện, nước...",
+	"Cuối năm 1945, quân đội Pháp quay lại Đông Dương. Sau những thương lượng không thành, Chiến tranh Đông Dương bùng nổ vào tháng 12 năm 1946 và thành phố Hà Nội nằm trong vùng kiểm soát của người Pháp. Năm 1954, chiến thắng Điện Biên Phủ giúp Việt Minh kiểm soát toàn bộ miền Bắc Việt Nam, Hà Nội tiếp tục giữ vị trí thủ đô của Việt Nam Dân chủ Cộng hòa. Ngày 30/9/1954, Việt Nam Dân chủ Cộng hòa và Pháp ký Hiệp định chuyển giao Hà Nội về quân sự; ngày 2/10, ký Hiệp định chuyển giao Hà Nội về hành chính.",
+	"Sau chiến tranh, Hà Nội tiếp tục giữ vai trò thủ đô của quốc gia Việt Nam thống nhất. Hà Nội đã được UNESCO trao danh hiệu: \"Thành phố vì hòa bình\" vào ngày 17 tháng 6 năm 1999. Năm 2000, Chủ tịch nước Trần Đức Lương đã tặng thưởng thành phố Hà Nội danh hiệu \"Thủ đô anh hùng\" vào ngày 4 tháng 10. Với tuổi đời hơn 1000 năm, Hà Nội chính là thủ đô lâu đời nhất trong 11 thủ đô của các quốc gia thuộc khu vực Đông Nam Á. Những cao ốc mọc lên ở khu vực nội ô và các trung tâm công nghiệp cũng được xây dựng ở những huyện ngoại thành. Sự phát triển cũng kéo theo những hệ lụy. Do không được quy hoạch tốt, giao thông thành phố thường xuyên ùn tắc khi số lượng xe máy tăng cao. Nhiều khu phố phải chịu tình trạng ngập úng mỗi khi mưa lớn. Mật độ dân số quá cao khiến những dân cư nội ô phải sống trong tình trạng chật chội và thiếu tiện nghi.",
+];
+
+var image =
+[
+	"http://hanoi.vietnamplus.vn/avatar.aspx?ID=112&at=0&ts=300&lm=634060813543400000",
+	"http://sohanews.sohacdn.com/thumb_w/660/2017/anhht-1484627812053-0-0-413-666-crop-1484627827371.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/9/9a/%C4%90%E1%BB%81n_Ng%E1%BB%8Dc_S%C6%A1n_1884.jpg",
+	"https://upload.wikimedia.org/wikipedia/commons/3/3c/Hanoi_rue_Paul_Bert.jpg",
+	"https://www.hanoi.gov.vn/image/image_gallery?img_id=2003102815732"
+];
+
+var image_title =
+[
+	"Thành Cổ Loa ở huyện Đông Anh, Hà Nội",
+	"Hoàng Thành Thăng Long",
+	"Đền Ngọc Sơn",
+	"Rue Paul Bert (nay là Phố Tràng Tiền)",
+	"Hà Nội nhìn từ trên cao"
+];
+
+function changePage(index) {
+	$("#title").text(title[index]);
+	$("#text").text(text[index]);
+	$("#image-title").text(image_title[index]);
+	$("#image").attr({
+		src: image[index]
+	});
+
+	$(".chossed").removeClass("chossed");	 
+	$("#nav-item" + index).addClass("chossed");
+}
